655a9474d96e806bca4f103627f6af5c98b97158
Shrikant <shrikant.awhad.58@gmail.com>
2024-02-09 12:56:21 +0530

    Spring Security with JWT Token

---
+++ src/main/java/com/ey/springboot3security/SpringBoot3SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ey.springboot3security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBoot3SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3SecurityApplication.class, args);
+    }
+
+}
